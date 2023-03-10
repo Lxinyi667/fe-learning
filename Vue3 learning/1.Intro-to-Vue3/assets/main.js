be93abdd2b2f4ce364fbcca7515b2f1fd97ce3db
Lxinyi667 <1716186890@qq.com>
2023-03-10 15:39:59 +0800

    Cloud Classroom Exercise

--- Vue3 learning/1.Intro-to-Vue3/assets/main.js
+++ Vue3 learning/1.Intro-to-Vue3/assets/main.js
@@ -10,7 +10,62 @@ const app = Vue.createApp({
             image:'./images/socks_green.jpg',
             url:'https://cn.vuejs.org/',
             inStock:true,
-            inventory:100
+            count:0,
+            details:[
+                {
+                  
+                    name:'Green Socks',
+                    image1:'./images/socks_green.jpg',
+                    flag:true,
+                },
+                {
+                   
+                    name:'Blue Socks',
+                    image1:'./images/socks_blue.jpg',
+                    flag:false,
+                }
+            ],
+            clouds:[
+                {
+                    imageP:'./images/SpringBoot.jpeg',
+                    imageT:'./images/tiao.png',
+                    imageL:'./images/lock.png',
+                    show:false,
+                    imageH:'./images/head.jpg',
+                    tname:'许莫淇',
+                    cname:'后端工程化开发',
+                    class:'软件2242 Web2班',
+                    flag:false,
+                },
+                {
+                    imageP:'./images/Vue3.jpg',
+                    imageT:'./images/tiao.png',
+                    imageL:'./images/lock.png',
+                    show:false,
+                    imageH:'./images/head.jpg',
+                    tname:'许莫淇',
+                    cname:'前端工程化开发',
+                    class:'软件2242 Web2班',
+                    flag:false,
+                },
+                {
+                    imageP:'./images/Web.jpeg',
+                    imageT:'./images/tiao.png',
+                    imageL:'./images/lock.png',
+                    // show:false,
+                    imageH:'./images/head.jpg',
+                    tname:'许莫淇',
+                    cname:'Web应用开发',
+                    class:'软件2242 Web2班',
+                    flag:true,
+                }
+            ]
+        }
+    },
+    methods:{
+        changeShow(index){
+            this.clouds[index].show =! this.clouds[index].show;
+            console.log(this.clouds[index].show);
         }
     }
 })
