085cbf2490ecc5bd74be5b9e9f1c9d05bb55f689
Lxinyi667 <1716186890@qq.com>
2023-03-12 00:56:12 +0800

    微信小程序云班课首页

---
+++ 云班课首页/miniprogram/pages/index/index.ts
@@ -0,0 +1,61 @@
+// index.ts
+// 获取应用实例
+const app = getApp<IAppOption>()
+Page({
+  data: {
+    list:[
+      {
+        imageP:'https://public-cdn-oss.mosoteach.cn/mssvc/cover/2023/02/e81eb0929b869d2098cb441da709ae66.jpeg?x-oss-process=style/s300x300',
+        imageT:'/miniprogram/images/tiao.png',
+        imageL:'https://static-cdn-oss.mosoteach.cn/mosoteach2/common/images/index_creat_icon_end.png',
+        show:false,
+        imageH:'https://public-cdn-oss.mosoteach.cn/avatar/2020/01/a2f12c73eed9ee74b87f8f05865a8d3b.jpg?v=1579922683&x-oss-process=style/s300x300',
+        tname:'许莫淇',
+        cname:'后端工程化开发',
+        class:'软件2242 Web2班',
+        flag:false,
+      },
+      {
+        imageP:'https://public-cdn-oss.mosoteach.cn/mssvc/cover/2023/02/effaea97669cecb86c6b9e9d2dce54cd.jpg?x-oss-process=style/s300x300',
+        imageT:'/miniprogram/images/tiao.png',
+        imageL:'https://static-cdn-oss.mosoteach.cn/mosoteach2/common/images/index_creat_icon_end.png',
+        show:false,
+        imageH:'https://public-cdn-oss.mosoteach.cn/avatar/2020/01/a2f12c73eed9ee74b87f8f05865a8d3b.jpg?v=1579922683&x-oss-process=style/s300x300',
+        tname:'许莫淇',
+        cname:'前端工程化开发',
+        class:'软件2242 Web2班',
+        flag:false,
+      },
+      {
+        imageP:'https://public-cdn-oss.mosoteach.cn/mssvc/cover/2023/02/319aa0a5a02386312000d5df99aa1a9d.jpeg?x-oss-process=style/s300x300',
+        imageT:'/miniprogram/images/tiao.png',
+        imageL:'https://static-cdn-oss.mosoteach.cn/mosoteach2/common/images/index_creat_icon_end.png',
+        // show:false,
+        imageH:'https://public-cdn-oss.mosoteach.cn/avatar/2020/01/a2f12c73eed9ee74b87f8f05865a8d3b.jpg?v=1579922683&x-oss-process=style/s300x300',
+        tname:'许莫淇',
+        cname:'Web应用开发',
+        class:'软件2242 Web2班',
+        flag:true,
+      }
+    ]
+  },
+  methods:{
+   
+}
+  // // 事件处理函数
+  // bindViewTap() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs',
+  //   })
+  // },
+  // onLoad() {
+  //   // @ts-ignore
+  //   if (wx.getUserProfile) {
+  //     this.setData({
+  //       canIUseGetUserProfile: true
+  //     })
+  //   }
+  // },
+ 
+ 
+})
